9e36e48ed34a3345328234bd61f28da98988b16e
Oleg <coolcash@list.ru>
2023-11-08 20:43:40 +0300

    Fix line 59 - now user see if his guess higher or lower than riddle number.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randint
 
-print('Welcome to gues number game!')
+print('Welcome to "The guess the number game!"')
 
 
 def is_valid(num):
@@ -17,9 +17,9 @@ def is_valid(num):
 
 def check_guess(guess, riddle_num):
     if guess < riddle_num:
-        return 'Your number is lower. Try again'
+        return 'Your number is lower. Try again!'
     elif guess > riddle_num:
-        return 'Your number is higher. Try again'
+        return 'Your number is higher. Try again!'
     return True
 
 
@@ -34,10 +34,13 @@ def exit_game():
     exit()
 
 
+counter = 0
+
+
 def game():
     global counter
     counter = 0
-    border_num = input('Enter right border or "q" to quit game: ')
+    border_num = input('Enter right border number or "q" to quit game: ')
     border_num = is_valid(border_num)
     riddle = randint(1, border_num)
     # print(riddle)
@@ -48,13 +51,14 @@ def game():
         check = check_guess(guess_num, riddle)
         if check is True:
             print(f'Correct! Number of attempts: {counter}')
-            ask = input('Play again? "Y" or "N"')
+            ask = input('Play again? Press "Y" to continue or any other key to quit')
             if ask.lower() == 'y':
                 game()
             else:
                 exit_game()
         else:
-            print('Nope!')
+            print(check)
 
 
-game()
+if __name__ == "__main__":
+    game()
