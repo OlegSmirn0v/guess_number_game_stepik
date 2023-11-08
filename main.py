9733358ca872075afba2786b46bac3d832a7a5a9
Oleg <coolcash@list.ru>
2023-11-08 20:19:14 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from random import randint
+
+print('Welcome to gues number game!')
+
+
+def is_valid(num):
+    while True:
+        if num == 'q':
+            exit_game()
+        elif num.isdigit() is False or int(num) <= 0:
+            print(
+                f'Not number or lower than zero. Try again or press "q" to quit game.')
+            num = input()
+        else:
+            return int(num)
+
+
+def check_guess(guess, riddle_num):
+    if guess < riddle_num:
+        return 'Your number is lower. Try again'
+    elif guess > riddle_num:
+        return 'Your number is higher. Try again'
+    return True
+
+
+def create_random_number(border):
+    random_number = randint(1, border)
+    return random_number
+
+
+def exit_game():
+    print(f'Number of attempts: {counter}')
+    print('Bye')
+    exit()
+
+
+def game():
+    global counter
+    counter = 0
+    border_num = input('Enter right border or "q" to quit game: ')
+    border_num = is_valid(border_num)
+    riddle = randint(1, border_num)
+    # print(riddle)
+    while True:
+        guess_num = is_valid(
+            input(f'Take a guess between one and {border_num} '))
+        counter += 1
+        check = check_guess(guess_num, riddle)
+        if check is True:
+            print(f'Correct! Number of attempts: {counter}')
+            ask = input('Play again? "Y" or "N"')
+            if ask.lower() == 'y':
+                game()
+            else:
+                exit_game()
+        else:
+            print('Nope!')
+
+
+game()
